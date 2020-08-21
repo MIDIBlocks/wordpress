4999547a304f7b10d7a2afff7c97fe5e663baf43
PatternTavern <oz@patterntavern.com>
2020-08-21 13:12:56 -0700

    Adds jQuery polyfill for WPBakery

--- wp-content/themes/Impreza-child/functions.php
+++ wp-content/themes/Impreza-child/functions.php
@@ -1,2 +1,12 @@
 <?php
-/* Custom functions code goes here. */
+
+/**
+ * Polyfills
+ * @fixme Remove this when WPBakery/Impreza are updated
+ */
+function globalScripts () {
+  wp_add_inline_script('jquery', ';$ = jQuery;', 'after');
+  wp_enqueue_script('jquery-polyfills', get_stylesheet_directory_uri() . '/lib/jquery.live-polyfill/dist/jquery.live-polyfill.min.js', ['jquery']);
+}
+add_action('admin_enqueue_scripts', 'globalScripts');
+add_action('wp_enqueue_scripts', 'globalScripts');
