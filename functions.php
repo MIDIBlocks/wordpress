c1fce2d297510ff7f7f069d40f6665c19622883f
Oz Ramos <midiblocks@gmail.com>
2020-08-29 23:09:13 -0700

    Removes jquery shim

--- functions.php
+++ functions.php
@@ -8,7 +8,6 @@ include get_stylesheet_directory() . '/cpts/dailies.php';
  */
 function globalScripts () {
   wp_add_inline_script('jquery', ';$ = jQuery;', 'after');
-  wp_enqueue_script('jquery-polyfills', get_stylesheet_directory_uri() . '/lib/jquery.live-polyfill/dist/jquery.live-polyfill.min.js', ['jquery']);
 }
 add_action('admin_enqueue_scripts', 'globalScripts');
 add_action('wp_enqueue_scripts', 'globalScripts');
@@ -17,7 +16,5 @@ add_action('wp_enqueue_scripts', 'globalScripts');
  * Theme setup
  */
 add_action('wp_enqueue_scripts', function () {
-  wp_enqueue_style('theme', get_stylesheet_directory_uri() . '/style.css', null,
-    '08-21-20-4'
-  );
+  wp_enqueue_style('theme', get_stylesheet_directory_uri() . '/style.css', null, '20-08-29-1');
 });
